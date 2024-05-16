13250d8ad86d16f9cfe161bbaf806784b156db72
borajin <borajin.soft@gmail.com>
2024-05-16 23:01:34 +0900

    ✨ feat: 메인페이지 낙서 이미지 추가

--- src/routes/master-mock-exam/+page.svelte
+++ src/routes/master-mock-exam/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import WarchiveSign from '$lib/images/mockexam/systems/warchive_sign.png';
+
 	import BaseHead from '$components/BaseHead.svelte';
 	import Cover from '$components/mockexam/Cover.svelte';
 	import Footer from '$components/Footer.svelte';
@@ -6,11 +8,36 @@
 	export let showAutioMessage = false;
 </script>
 
-<BaseHead
-	title="Warchive: 여성서사 고인물 모의고사"
-	image="https://i.ibb.co/SRQVvJX/master.png"
-/>
+<BaseHead title="Warchive: 여성서사 고인물 모의고사" image="https://i.ibb.co/SRQVvJX/master.png" />
 
-<Cover />
+<section>
+	<img src={WarchiveSign} alt="와카이브 낙서" />
 
-<Footer />
+	<Cover />
+
+	<Footer />
+</section>
+
+<style>
+	section {
+		position: relative;
+		width: 100%;
+		max-width: 1000px;
+		padding: 1rem;
+	}
+
+	img {
+		opacity: 0.5;
+		width: 40%;
+		position: absolute;
+		top: -100px;
+		left: 50px;
+	}
+
+	@media (max-width: 700px) {
+		img {
+			left: 10px;
+			top: -50px;
+		}
+	}
+</style>
